9f6b29e1835c26a9f93990cc00b6d0bce2fb03ef
MiniT0on <70152813+MiniT0on@users.noreply.github.com>
2022-02-24 12:38:35 -0500

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,10 @@
-console.log("Hola mundo")
+const Labels = {
+    ["Una_Letra"]: document.getElementById('Una_Letra'),
+    ["Resto_Letras"]: document.getElementById('Resto_Letras')
+};
+
+window.addEventListener('load', ()=>{
+    Labels["Una_Letra"].textContent = STRINGS.HTML.LabelsID.Una_Letra;
+    Labels["Resto_Letras"].textContent = STRINGS.HTML.LabelsID.Resto_Letras;
+});
+
