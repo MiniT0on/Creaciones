888c0a28f3d8471ca9645e31e829c50a096ba047
MiniT0on <70152813+MiniT0on@users.noreply.github.com>
2022-02-25 12:08:25 -0500

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,19 @@
+// Solo par labels
 const Labels = {
     ["Una_Letra"]: document.getElementById('Una_Letra'),
     ["Resto_Letras"]: document.getElementById('Resto_Letras')
 };
 
+const DataList_OPCIONES = [ "Roblox Studio", "HTML", "Aplicaciones de escritorio",  ]
+
 window.addEventListener('load', ()=>{
+    // Colocando texto dentro de dos LABELS al cargar la ventana
     Labels["Una_Letra"].textContent = STRINGS.HTML.LabelsID.Una_Letra;
     Labels["Resto_Letras"].textContent = STRINGS.HTML.LabelsID.Resto_Letras;
-});
 
+    // Colocando opciones de busqueda en una DataList cuando la ventana cargue
+    STRINGS.HTML.DataList.Buscar_SECCION.innerHTML = '';
+    DataList_OPCIONES.forEach(elemento => {
+        STRINGS.HTML.DataList.Buscar_SECCION.innerHTML += `<option value="${elemento}">`;
+    })
+});
